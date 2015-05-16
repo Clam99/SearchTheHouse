0f18d566d8de4be10f9e697a320cb8b8d057c2c2
Sam Noyes <eliotman24@comcast.net>
2015-05-16 08:50:14 -0400

    TestWalker now opens a window successfully

--- MainProj/src/Driver.java
+++ MainProj/src/Driver.java
@@ -1,6 +1,7 @@
 /**
  * Created by samnoyes on 5/14/15.
  */
+
 public class Driver {
 
     public static void main(String args[]) {
--- MainProj/src/TestWalker.java
+++ MainProj/src/TestWalker.java
@@ -1,7 +1,37 @@
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import java.awt.event.KeyListener;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.util.glu.GLU.gluLookAt;
+import static org.lwjgl.util.glu.GLU.gluPerspective;
+
+/**
+ * Created by samnoyes on 5/14/15.
+ */
 
 public class TestWalker {
 
-    public TestWalker() {
-        System.out.println("Created!");
-    }
+
+
+        public TestWalker()  {
+            try {
+                start();
+            }
+            catch (LWJGLException e) {
+                e.printStackTrace();
+            }
+        }
+        public void start() throws org.lwjgl.LWJGLException {
+            Display.setDisplayMode(new DisplayMode(800, 800));
+            Display.create();
+            gluPerspective(80f, 1.0f, -100f, 1800f);
+            gluLookAt(-500,0,500,0,0,0,0,1,0);
+            glEnable(GL_DEPTH_TEST);
+
+        }
 }
