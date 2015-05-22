15288f4cd7a4f4f2e30815d187bb5d81b1fc0fea
jstraightman <jstratmann@exeter.edu>
2015-05-22 09:47:14 -0400

    Update xyWall.java
    actually added stuff

--- MainProj/src/xyWall.java
+++ MainProj/src/xyWall.java
@@ -1,1 +1,60 @@
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.util.glu.GLU.*;
+
+public class xyWall implements Wall{
+	
+	private float x,y,posx,posz;
+	private boolean door;
+	
+	public xyWall(float width, float height, float posx, float posz, boolean door){
+		x=width;
+		y=height;
+		this.door=door;
+		this.posx=posx;
+		this.posz=posz;
+	}
+	
+	public void draw() {
+		
+		if(!door){
+		glBegin(GL_QUADS);
+		glVertex3f(posx, 0f, posz);
+		glVertex3f(posx, y, posz);
+		glVertex3f(posx+x, y, posz);
+		glVertex3f(posx+x, 0f, posz);
+		}
+		else{
+		glBegin(GL_QUADS);
+		glVertex3f(posx, 0f, posz);
+		glVertex3f(posx, y, posz);
+		glVertex3f(posx+x/3, y, posz);
+		glVertex3f(posx+x/3, 0, posz);
+		glEnd();
+		glBegin(GL_QUADS);
+		glVertex3f(posx+x/3, y, posz);
+		glVertex3f(posx+2*x/3, y, posz);
+		glVertex3f(posx+2*x/3, 3*y/4, posz);
+		glVertex3f(posx+x/3, 3*y/4, posz);
+		glEnd();
+		glBegin(GL_QUADS);
+		glVertex3f(posx+2*x/3, y, posz);
+		glVertex3f(posx+x, y, posz);
+		glVertex3f(posx+x, 0, posz);
+		glVertex3f(posx+2*x/3, 0, posz);
+		}
+		glEnd();
+	}
+	
+	public boolean testCollision(float cx, float cz) {
+		if ((cx>=posx && cx<=(posx+x)) && cz==posz) {
+			if (door && cx>(posx+x/3) && cz<(posx+2*x/3)) return false;
+			else return true;
+		}
+		else return false;
+	}
+}
 
