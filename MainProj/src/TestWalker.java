dc3ddaee0e70625605210843b42ed2c88c2705d3
jstraightman <jstratmann@exeter.edu>
2015-05-25 12:31:41 -0400

    house is now displayed

--- MainProj/src/TestWalker.java
+++ MainProj/src/TestWalker.java
@@ -5,6 +5,7 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL11.glEnd;
@@ -34,6 +35,28 @@ public class TestWalker {
         gluPerspective(50f, 1.0f, -1000f, 1000f);
         gluLookAt(0,0,10,0,0,0,0,1,0);
         glEnable(GL_DEPTH_TEST);
+        ArrayList<Wall> walls = new ArrayList<Wall>(20);
+    	walls.add(new xyWall(17,3,1,1, false));
+    	walls.add(new xyWall(4,3,1,5, true));
+    	walls.add(new xyWall(5,3,5,5, true));
+    	walls.add(new xyWall(3,3,10,5, false));
+    	walls.add(new xyWall(5,3,13,5, true));
+    	walls.add(new xyWall(5,3,5,8, true));
+    	walls.add(new xyWall(3,3,10,8, false));
+    	walls.add(new xyWall(5,3,13,8, true));
+    	walls.add(new xyWall(4,3,1,12, true));
+    	walls.add(new xyWall(13,3,5,12, false));
+    	walls.add(new zyWall(11,3,1,1, false));
+    	walls.add(new zyWall(4,3,5,1, false));
+    	walls.add(new zyWall(4,3,10,1, true));
+    	walls.add(new zyWall(4,3,13,1, true));
+    	walls.add(new zyWall(11,3,18,1, false));
+    	walls.add(new zyWall(3,3,10,5, true));
+    	walls.add(new zyWall(3,3,13,5, true));
+    	walls.add(new zyWall(4,3,5,8, false));
+    	walls.add(new zyWall(4,3,10,8, false));
+    	walls.add(new zyWall(4,3,13,8, true));
+    	
         while (!Display.isCloseRequested()) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -45,27 +68,56 @@ public class TestWalker {
             boolean down = Keyboard.isKeyDown(Keyboard.KEY_DOWN);
             boolean right = Keyboard.isKeyDown(Keyboard.KEY_RIGHT);
             boolean left = Keyboard.isKeyDown(Keyboard.KEY_LEFT);
-            if (moveLeft) l.setVx(l.getVx() + .1f);
-            if (moveRight) l.setVx(l.getVx() - .1f);
-            if (forward) l.setVz(l.getVz() + .1f);
-            if (back) l.setVz(l.getVz() - .1f);
+            if (moveLeft) l.setVx(l.getVx() + .01f);
+            if (moveRight) l.setVx(l.getVx() - .01f);
+            if (forward) l.setVz(l.getVz() + .01f);
+            if (back) l.setVz(l.getVz() - .01f);
             if (left) {
                 //l.setVx(l.getVx() - .03f);
-                l.setRotation(l.getRotation()+.005);
+                l.setRotation(l.getRotation()+.002);
             }
             if (right) {
                 //l.setVx(l.getVx() + .03f);
-                l.setRotation(l.getRotation()-.005);
+                l.setRotation(l.getRotation()-.002);
             }
-            if (up && l.getViewAngle()<45*(Math.PI/180)) l.setViewAngle(l.getViewAngle()+.005);
-            if (down && l.getViewAngle()>-45*(Math.PI/180)) l.setViewAngle(l.getViewAngle()-.005);
+            if (up && l.getViewAngle()<45*(Math.PI/180)) l.setViewAngle(l.getViewAngle()+.001);
+            if (down && l.getViewAngle()>-45*(Math.PI/180)) l.setViewAngle(l.getViewAngle()-.001);
             if (l.isLookingAt(0,0,0) != isLookingAtOrigin) {
                 isLookingAtOrigin = !isLookingAtOrigin;
                 System.out.println(l.isLookingAt(0,0,0));
             }
+            
+            for (Wall wall: walls) {
+            	if(wall.testCollision(l.getX(), l.getZ())) {
+            		if (moveRight) l.setVx(l.getVx()+.02f);
+            		if (moveLeft) l.setVx(l.getVx()-.02f);
+            		if (forward) l.setVz(l.getVz()-.02f);
+            		if (back) l.setVx(l.getVx()+.02f);
+            	}
+            }
             l.updatePosition();
             updateGLU(l);
-            drawFloor();
+            glColor3f(1,1,1);
+    		for (Wall wall : walls){
+    			glColor3f(wall.getPosX()/20, 1, wall.getPosZ()/20);
+    			wall.draw();
+    			//x++;
+    		}
+    		glColor3f(1,.2f,.2f);
+    		glBegin(GL_QUADS);
+    		glVertex3f(1,0,1);
+    		glVertex3f(18,0,1);
+    		glVertex3f(18,0,12);
+    		glVertex3f(1,0,12);
+    		glEnd();
+    		glColor3f(1,1,1);
+    		glBegin(GL_QUADS);
+    		glVertex3f(1,0,1);
+    		glVertex3f(-1,0,1);
+    		glVertex3f(-1,0,-1);
+    		glVertex3f(1,0,-1);
+    		glEnd();
+            //drawFloor();
             Display.update();
         }
         Display.destroy();
