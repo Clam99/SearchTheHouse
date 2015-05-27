5522b99ddc6197ccebb2516a237f03054b0f50a6
jstraightman <jstratmann@exeter.edu>
2015-05-27 17:01:04 -0400

    Update DisappearingObject.java

--- MainProj/src/DisappearingObject.java
+++ MainProj/src/DisappearingObject.java
@@ -1,14 +1,13 @@
-/**
- * Created by smurphy on 5/26/15.
- */
 public class DisappearingObject extends PlayerObject {
+	public final float r=1, g=1, b=0;
 
     public DisappearingObject(float xp, float yp, float zp, float size) {
-        super(xp,yp,zp,size);
+        super(xp,yp,zp,1,1,0,size);
     }
 
-    public void find(PlayerLogic l) {
+    public boolean find(PlayerLogic l) {
         wasFound = true;
         isDisplayed = false;
+        return false;
     }
 }
