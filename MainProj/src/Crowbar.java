fed3aac39e1b7e56efad08748a43591b9006608c
jstraightman <jstratmann@exeter.edu>
2015-05-27 17:00:18 -0400

    Update Crowbar.java

--- MainProj/src/Crowbar.java
+++ MainProj/src/Crowbar.java
@@ -1,8 +1,11 @@
-/**
- * Created by smurphy on 5/26/15.
- */
-public class Crowbar extends DisappearingObject {
+public class Crowbar extends PlayerObject {
     public Crowbar(float xp, float yp, float zp, float size) {
-        super(xp,yp,zp,size);
+        super(xp,yp,zp,0,0,0,size);
+    }
+    
+    public boolean find(PlayerLogic l) {
+        wasFound = true;
+        isDisplayed = false;
+        return true;
     }
 }
