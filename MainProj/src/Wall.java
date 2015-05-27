a04bfb492367f07153245e290a053b42b04be3b4
jstraightman <jstratmann@exeter.edu>
2015-05-26 22:26:33 -0400

    added open() method

--- MainProj/src/Wall.java
+++ MainProj/src/Wall.java
@@ -1,9 +1,13 @@
 public interface Wall {
+
+	public abstract float getPosX();
 	
+	public abstract float getPosZ();
+	
+	public abstract void open();
+
 	public abstract void draw();
 	
 	public abstract boolean testCollision(float x, float z);
 
-	public abstract float getPosX();
-	public abstract float getPosZ();
 }
