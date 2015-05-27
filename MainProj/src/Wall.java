c771cf0e0d2eb9784d5b5a5dd1cd714ee5bb0dbf
jstraightman <jstratmann@exeter.edu>
2015-05-27 17:11:11 -0400

    Update Wall.java

--- MainProj/src/Wall.java
+++ MainProj/src/Wall.java
@@ -1,14 +1,29 @@
 public interface Wall {
+
+	public abstract float getPosX();
+	
+	public abstract float getPosZ();
+	
+	public abstract void addX(float x);
+	
+	public abstract void addZ(float z);
+	
+	public abstract float getWidth();
+	
+	public abstract float getHeight();
+
+	public abstract Vector getSurfaceVector();
+	
+	public abstract Vector getNormalVector();
 	
 	public abstract void open();
+	
 	public abstract void close();
 
 	public abstract void draw();
 	
+	public abstract Wall copy();
+	
 	public abstract boolean testCollision(float x, float z);
 
-	public abstract float getPosX();
-	public abstract float getPosZ();
-	public abstract Vector getSurfaceVector();
-	public abstract Vector getNormalVector();
 }
