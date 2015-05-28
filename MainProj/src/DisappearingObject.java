f6d61817eca7aeb64c8a56850bb49b2532fb78db
jstraightman <jstratmann@exeter.edu>
2015-05-28 15:32:03 -0400

    Update DisappearingObject.java

--- MainProj/src/DisappearingObject.java
+++ MainProj/src/DisappearingObject.java
@@ -1,8 +1,7 @@
 public class DisappearingObject extends PlayerObject {
-	public final float r=1, g=1, b=0;
 
     public DisappearingObject(float xp, float yp, float zp, float size) {
-        super(xp,yp,zp,1,1,0,size);
+        super(xp,yp,zp,1,1,0,size); //construct PlayerObjet with certain color
     }
 
     public void find(PlayerLogic l) {
