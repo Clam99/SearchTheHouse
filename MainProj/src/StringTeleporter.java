3991de8ee86021d4d5d8003b8aec54097a7bdd3c
jstraightman <jstratmann@exeter.edu>
2015-05-28 15:35:29 -0400

    Update StringTeleporter.java

--- MainProj/src/StringTeleporter.java
+++ MainProj/src/StringTeleporter.java
@@ -1,10 +1,8 @@
-/**
- * Created by Sam Noyes and Jan Stratmann on 5/27/15.
- */
 public class StringTeleporter extends Teleporter {
     String code;
     TargetDelegate target;
 
+    //this teleporter requires a password to work
     public StringTeleporter(float xp, float zp, float tx, float tz, float size, String code, TargetDelegate t) {
         super(xp, zp, tx, tz, size);
         this.code = code;
